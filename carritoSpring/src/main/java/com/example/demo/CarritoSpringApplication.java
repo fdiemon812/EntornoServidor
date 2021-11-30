e2e24d82be847e91261480647b032aa89ba1c0a0
estudiante <flavio.dediego@gmail.com>
2021-11-30 10:57:50 +0100

    Spring

---
+++ carritoSpring/src/main/java/com/example/demo/CarritoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoSpringApplication.class, args);
+	}
+
+}
