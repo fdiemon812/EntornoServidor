6d8989095c8607e2e45c4d82f493e12eae11edf8
estudiante <flavio.dediego@gmail.com>
2021-12-09 10:13:39 +0100

    Proyecto SPRING

--- carritoSpring/src/main/java/com/example/demo/controller/LoginController.java
+++ carritoSpring/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,19 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.model.Pedido;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 
 @Controller
 public class LoginController {
 
-	
+	@Autowired
+	private HttpSession sesion;
 	
 	@Autowired
 	private UsuarioService userServ;
 	
 	//vincular con página de inicio
-	@GetMapping("/login")
+	@GetMapping({"/", "/login"})
 	public String login(Model model) {
 		model.addAttribute("usuario", new Usuario());
 
@@ -27,23 +31,49 @@ public class LoginController {
 	}
 	
 	
-	@PostMapping("/login/submit")
-	public String envio(@ModelAttribute("usuario") Usuario usuario) {
-		userServ.compruebaUsuario(usuario.getUser(), usuario.getPassword());
-		System.out.println(usuario.getPassword());
-		System.out.println(usuario.getUser());
-		return "submit";
+	@PostMapping("/login/catalogo")
+	public String envio(@ModelAttribute("usuario") Usuario usuario, Model model) {
+		
+		model.addAttribute("pedido", new Pedido());
+		System.out.println("postCatalogo");
+		System.out.println(sesion.getAttribute("user"));
+
+		String result="catalogo";
+		boolean isUser= userServ.compruebaUsuario(usuario.getUser(), usuario.getPassword());
+		
+		if(isUser) {
+			sesion.setAttribute("user", usuario.getUser());
+			sesion.setMaxInactiveInterval(10);
+		}else {
+			result="redirect:/login";
+		}
+		
+		return result;
 		
 	}
-	//falta return
-//	@PostMapping("/index.html")
-//	public String inicioSesion(@ModelAttribute("usuario") Usuario usuario) {
-//		System.out.println(userServ.compruebaUsuario(usuario.getNombre(), usuario.getPassword()));
-//		System.out.println(usuario.getPassword());
-//		
-//		return "index";
-//		
-//	}
+	
+	
+	@PostMapping("/login/pedido")
+	public String resumenPedido(@ModelAttribute("pedido") Pedido pedido) {
+		
+		
+		
+		return "";
+		
+	}
+	
+	
+	@GetMapping({"/login/catalogo","/login/catalogo/"})
+	public String forzarInicio(@ModelAttribute("usuario") Usuario usuario) {
+		System.out.println("getCatalogo");
+		System.out.println(sesion.getAttribute("user"));
+
+	
+		
+		return "redirect:/login";
+		
+	}
+	
 	
 	
 }
