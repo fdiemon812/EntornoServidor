136b47ad6160bc489a4c30c147b86a7fa592710a
estudiante <flavio.dediego@gmail.com>
2022-01-13 10:26:38 +0100

    Add WebVenta, implementando LineaPedido

---
+++ SpringWebVentaCarrito/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Pedido;
+import com.example.demo.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String >{
+
+}
---
+++ SpringWebVentaCarrito/src/main/java/com/example/demo/service/PedidoService.java
@@ -0,0 +1,169 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.LineaPedido;
+import com.example.demo.model.Pedido;
+import com.example.demo.model.Producto;
+import com.example.demo.model.Usuario;
+
+@Service
+public class PedidoService {
+
+	private ArrayList<Producto> listaProductosDefecto = new ArrayList<>();
+
+	/**
+	 * Recibe la posicion de un producto en la BBDD o ID, el pedido donde lo vas a 
+	 * add y la cantidad que quieres add de ese producto.
+	 * @param posicion
+	 * @param pedido
+	 * @param cantidad
+	 */
+	public void addPedido(int posicion, Pedido pedido, int cantidad) {
+		
+			
+//			HashMap<Producto, Integer> map= pedido.getListaProductos();
+//			
+//
+//			
+//			if(map.containsKey(listaProductosDefecto.get(posicion))) {
+//				
+//				int cantidadOld=map.get(listaProductosDefecto.get(posicion));
+//				int cantidadNew= cantidadOld+cantidad;
+//				map.put(listaProductosDefecto.get(posicion), cantidadNew);
+//
+//				
+//			}else {
+//				
+//				map.put(listaProductosDefecto.get(posicion), cantidad);
+//			}
+		
+		
+		
+//		List<Producto> productos=pedido.getListaProductos();
+//		
+//		if(productos.contains(listaProductosDefecto.get(posicion))) {
+//			
+//			int cantidadOld=productos.get(posicion).getCantidad();
+//			int cantidadNew=cantidadOld+cantidad;
+//			productos.get(posicion).setCantidad(cantidadNew);
+//			
+//		}else {
+//			Producto prod = new Producto(listaProductosDefecto.get(posicion).getNombre(), 
+//					listaProductosDefecto.get(posicion).getPrecio(),
+//					listaProductosDefecto.get(posicion).getId(),
+//					listaProductosDefecto.get(posicion).getImg());
+//				
+//			prod.setCantidad(cantidad);
+//			productos.add(prod);
+//	}
+		
+		
+		
+		
+		LineaPedido nuevaLinea= new LineaPedido(listaProductosDefecto.get(posicion), pedido);
+		
+		ArrayList<LineaPedido> lineasPedido=(ArrayList<LineaPedido>) pedido.getListaProductos();
+			
+		if(lineasPedido.contains(nuevaLinea)) {
+			
+			int cantidadOld=lineasPedido.get
+			
+		}else {
+			nuevaLinea.setCantidad(cantidad);
+			lineasPedido.add(nuevaLinea);
+			
+			
+		}
+	}
+	
+	/**
+	 * Recibe un pedido y devuelve el precio total. 
+	 * @param pedido
+	 * @return Double precioToptal
+	 */
+	public Double calculaPrecioTotal(Pedido pedido) {
+		Double result=0.0;
+		
+		
+		for (Producto producto : pedido.getListaProductos()) {
+			
+			result=result+producto.getPrecio()*producto.getCantidad();
+		}
+		
+		
+//		for (Map.Entry<Producto, Integer> producto : pedido.listaProductos.entrySet()) {
+//			
+//			result=result+(( producto.getKey()).getPrecio())*producto.getValue();
+//		}
+		
+		return result;
+		
+	}
+	
+	/**
+	 *  Recibe un usuario y un id de pedido. Borra el pedido con dicho ID del usuario recibido. 
+	 * @param usuario
+	 * @param id
+	 */
+	public void borrarPedido(Usuario usuario, int id) {
+		Pedido pedido = new Pedido(id);
+		
+		
+		usuario.getListaPedidos().remove(pedido);
+		
+	}
+	
+	/**
+	 * Recibe un id de pedido y un usuario. Devuelve el pedido dentro del usuario, con dicho ID.
+	 * @param id
+	 * @param usuario
+	 * @return Pedido
+	 */
+	public Pedido findPedido(int id, Usuario usuario) {
+		Pedido pedido = new Pedido(id);
+		ArrayList<Pedido> listaPedidos = new ArrayList<>(usuario.getListaPedidos());
+		
+		return  listaPedidos.get(listaPedidos.indexOf(pedido));
+	}
+	
+	/**
+	 * Recibe un usuario. Devuelve
+	 * @param usuario
+	 * @return
+	 */
+	public Pedido findPedido(Usuario usuario) {
+		return usuario.getListaPedidos().get(0);
+		
+	}
+	/**
+	 * Inicia una lista de productos por defecto. 
+	 */
+	@PostConstruct
+	public void intiProductos() {
+		listaProductosDefecto.addAll(Arrays.asList(
+				new Producto("Mordedor-A", 20.0, 0, "/img/pesa.jpg"),
+				new Producto("Mordedor-B", 15.0, 1, "/img/pesa2.png"),
+				new Producto("Mordedor-C", 10.0, 2, "/img/pesa3.jpg")));
+	}
+	
+	/**
+	 * Devuelve los productos recogidos en nuestra lista por defecto
+	 * @return
+	 */
+	public ArrayList<Producto> findAll() {
+		
+		return listaProductosDefecto;
+		
+		
+	}
+	
+}
